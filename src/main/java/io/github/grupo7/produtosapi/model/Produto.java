7b33f75c7efa3cd1162f2bfb194ea71f46c992c0
AndreLuisGomes <andreluisgomes09@gmail.com>
2024-11-22 23:34:27 -0300

    Upando projeto

---
+++ src/main/java/io/github/grupo7/produtosapi/model/Produto.java
@@ -0,0 +1,66 @@
+package io.github.grupo7.produtosapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+// POJO -> Plain Old Java Object
+
+@Entity
+public class Produto {
+
+    @Id
+    @Column
+    private String id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private Double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
---
+++ src/main/java/io/github/grupo7/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package io.github.grupo7.produtosapi.repository;
+
+import io.github.grupo7.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
